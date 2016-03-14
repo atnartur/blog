708df45d38f6d745a5a1695463ecc1297948ee44
atnartur <i@atnartur.ru>
2016-03-14 10:54:53 +0300

    + hexo

--- README.md
+++
@@ -1,4 +0,0 @@
-# [atnartur.ru](http://atnartur.ru)
-
-## License
-Copyright &copy; 2013-2016 Атнагулов Артур. Лицензия MIT.
---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+/**
+ * @author Artur Atnagulov (atnartur), ClienDDev team (clienddev.ru)
+*/
+
+var gulp = require('gulp');
+var less = require('gulp-less')
+var nano = require('gulp-cssnano');
+var rename = require('gulp-rename');
+
+gulp.task('css', function() {
+    gulp.src('themes/atnartur/source/styles/main.less')
+    	.pipe(less())
+    	.pipe(nano())
+    	.pipe(rename('main.min.css'))
+    	.pipe(gulp.dest('themes/atnartur/source/styles/'));
+});
+
+gulp.task('default',['css']);
