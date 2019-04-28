27989bd63eb9eee98206d215605ac5575ea6142a
atnartur <i@atnartur.ru>
2019-04-28 19:06:30 +0300

    = gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,28 @@
-/**
- * @author Artur Atnagulov (atnartur), ClienDDev team (clienddev.ru)
-*/
+const gulp = require('gulp');
+const less = require('gulp-less')
+const nano = require('gulp-cssnano');
+const rename = require('gulp-rename');
 
-var gulp = require('gulp');
-var less = require('gulp-less')
-var nano = require('gulp-cssnano');
-var rename = require('gulp-rename');
+const copyFaCss = () => gulp.src('./node_modules/font-awesome/css/*')
+    .pipe(gulp.dest('./themes/atnartur/source/libs/font-awesome/css/'));
 
-gulp.task('copy-fa-css', () => gulp.src([
-    './node_modules/font-awesome/css/*'
-]).pipe(gulp.dest('./themes/atnartur/source/libs/font-awesome/css/')));
+const copyFaFonts = () => gulp.src('./node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('./themes/atnartur/source/libs/font-awesome/fonts/'));
 
-gulp.task('copy-fa-fonts', () => gulp.src([
-    './node_modules/font-awesome/fonts/*'
-]).pipe(gulp.dest('./themes/atnartur/source/libs/font-awesome/fonts/')));
+const copyFromNpm = () => gulp.src([
+        './node_modules/highlightjs/highlight.pack.min.js',
+        './node_modules/highlightjs/styles/github.css',
+        './node_modules/bootstrap-3-typeahead/bootstrap3-typeahead.min.js'
+    ]).pipe(gulp.dest('./themes/atnartur/source/libs/'));
 
-gulp.task('copy-from-npm',  () => gulp.src([
-    './node_modules/highlightjs/highlight.pack.min.js',
-    './node_modules/highlightjs/styles/github.css',
-    './node_modules/bootstrap-3-typeahead/bootstrap3-typeahead.min.js'
-]).pipe(gulp.dest('./themes/atnartur/source/libs/')));
+const copy = gulp.parallel([copyFaCss, copyFaFonts, copyFromNpm]);
 
-gulp.task('copy', ['copy-fa-css', 'copy-fa-fonts', 'copy-from-npm']);
+const css = () => gulp.src('themes/atnartur/source/styles/_main.less')
+	.pipe(less())
+	.pipe(nano())
+	.pipe(rename('main.min.css'))
+	.pipe(gulp.dest('themes/atnartur/source/styles/'));
 
-gulp.task('css', function() {
-    gulp.src('themes/atnartur/source/styles/_main.less')
-    	.pipe(less())
-    	.pipe(nano())
-    	.pipe(rename('main.min.css'))
-    	.pipe(gulp.dest('themes/atnartur/source/styles/'));
-});
+module.exports.watch = () => gulp.watch('themes/atnartur/source/styles/*', [css]);
 
-gulp.task('watch', function() {
-    gulp.watch('themes/atnartur/source/styles/*', ['css']);
-});
-
-gulp.task('default', ['css', 'copy']);
+module.exports.default = gulp.parallel([css, copy]);
